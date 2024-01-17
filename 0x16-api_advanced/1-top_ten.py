3f54d788359b9142ce6d8f989eafaed78c8a2e41
Henry Arnold <devharnold@gmail.com>
2024-01-17 11:53:43 +0300

    Querying top ten hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Query the top ten hottest posts
+Liked for a given subreddit
+"""
+import requests
+
+def top_ten(subreddit):
+    """Working with the pagination function to query the first 10 hot posts"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "Linux:0x16.api.advanced:v1.0.0 (by /acceptable/top)"
+    }
+    params = {
+        "Limit": "10"
+    }
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("posts")
