b57e5499b9c7a91f047f989c952502cca3ef2cac
Henry Arnold <devharnold@gmail.com>
2024-01-17 23:18:43 +0300

    bug fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,6 @@
-#add libfoo.so path to LD_LIBRARY_PATH
+#corrected code -> bug fix
 
-file{'/etc/profile.d/custom_ld_library_path.sh'
-ensure => present,
-content => 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/path/to/libfoo',
-mode => '0644',
-}
-
-#restart apache service
-service {'apache2:
-ensure => running,
-subscribe => File['/etc/profile.d/custom_ld_library_path.sh'],
+exec { 'Find bugs and fix':
+    command => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+    provider => linux
 }
